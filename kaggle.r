dacda0f3740a9f86c0c89fe9a60444b0f54688e7
Cyuss <maamra.youcef@gmail.com>
2016-02-19 23:18:05 +0100

    changement dans kaggle.r

---
+++ kaggle.r
@@ -0,0 +1,22 @@
+library(ggmap)
+library(ggplot2)
+library(dplyr)
+
+# chemin vers le projet
+path <- "/media/youcef/3c1e8ee4-6478-457b-a5ff-0587a29a0078/Dropbox/crime_classification/"
+setwd(path)
+
+# charger les données
+train <- read.csv("train.csv")
+# charger la carte géographique de San Francisco
+map <- get_map("San Francisco", zoom = 12, color = "bw")
+
+# fonction pour filtrer les données selon la catégorie et les projeter sur la map
+map_crime <- function(crime_df, crime) {
+    filtered <- filter(crime_df, Category %in% crime)
+    plot <- ggmap(map, extent = 'device') + 
+        geom_point(data = filtered, aes(x = X, y = Y, color = Category), alpha = 0.6)
+    return(plot)
+}
+
+map_crime(train, c('SUICIDE', 'ASSAULT'))
