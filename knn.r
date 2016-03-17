5df36c87f45b1a282a1b27af278a0f96b7728c12
Cyuss <maamra.youcef@gmail.com>
2016-03-17 22:23:32 +0000

    enlever les lignes en plus

--- knn.r
+++ knn.r
@@ -33,11 +33,6 @@ y_train_scaled <- data.Normalization(train$y, type = "n1")
 hour_train_scaled <- data.Normalization(train$hour, type = "n1")
 month_train_scaled <- data.Normalization(train$month, type = "n1")
 
-#x_test_scaled <- data.Normalization(test$x, type = "n4")
-#y_test_scaled <- data.Normalization(test$y, type = "n4")
-#hour_test_scaled <- data.Normalization(test$hour, type = "n4")
-#month_test_scaled <- data.Normalization(test$month, type = "n4")
-
 train_model <- data.frame(category_predict = train$category_predict, 
                           x_scaled = x_train_scaled, 
                           y_scaled = y_train_scaled, 
@@ -45,30 +40,6 @@ train_model <- data.frame(category_predict = train$category_predict,
                           month_scaled = month_train_scaled)
 
 names(train_model) <- c("category_predict", "x_scaled", "y_scaled", "hour_scaled", "month_scaled")
-
-#test_model <- data.table(x_scaled = x_test_scaled, 
-                         #y_scaled = y_test_scaled, 
-                         #hour_scaled = hour_test_scaled,
-                         #month_scaled = month_test_scaled)
-
-#test_model <- data.table(category_predict = train$category_predict[570732:878049], 
-                          #x_scaled = x_train_scaled[570732:878049], 
-                          #y_scaled = y_train_scaled[570732:878049], 
-                          #hour_scaled = hour_train_scaled[570732:878049],
-                          #month_scaled = month_train_scaled[570732:878049])
-#test_model <- train_model[570732:878049, ]
-#train_model <- train_model[1:570731, ]
-
-#train_model_labels <- train_model$category_predict
-#test_model_labels <- test_model$category_predict
-
-#model_pred <- knn(train = train_model, 
-#                  test = test_model, 
-#                  cl = train_model_labels,
-#                  k = 50)
-
-
-
 
 # une autre partie
 subset = train_model[c(1:30000, 30024, 33954, 37298, 41097, 41980, 44479, 48707, 48837, 49306, 53715, 93717, 102637, 102645, 102678, 102919, 103517, 103712, 107734, 148476, 148476, 192191, 205046, 252094, 279792, 316491, 317527, 332821, 337881), ]
